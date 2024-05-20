a0a3d3d7595e6102631d5a2aa706ae9f13417511
Mukaddes KUCUK <mukaddeszorlu@gmail.com>
2024-05-21 00:22:30 +0200

    v1

---
+++ src/test/java/testngodev/pages/zerobankpage/C01.java
@@ -0,0 +1,15 @@
+package testngodev.pages.zerobankpage;
+
+public class C01 {
+    public static void main(String[] args) {
+
+
+        System.out.println(" yeni branch olusturuldu .Murvet HAnimin dikkatine!");
+        System.out.println(" yeni branch olusturuldu .Murvet HAnimin dikkatine!");
+        System.out.println(" yeni branch olusturuldu .Murvet HAnimin dikkatine!");
+        System.out.println(" yeni branch olusturuldu .Murvet HAnimin dikkatine!");
+        System.out.println(" yeni branch olusturuldu .Murvet HAnimin dikkatine!");
+
+
+    }
+}
